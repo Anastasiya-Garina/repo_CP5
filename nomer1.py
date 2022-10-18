e24659601724438dbf7256703c04b4024de10b83
Anastasiya-Garina <114314685+Anastasiya-Garina@users.noreply.github.com>
2022-10-18 19:14:57 +0300

    Add files via upload

--- nomer1.py
+++ nomer1.py
@@ -1,12 +1,12 @@
 #1
-def f(сс):
+def f(сс, numb):
     res = ''
-    while chiclo10 > 0:
-        a = chiclo10 % cc
+    while numb > 0:
+        a = numb % cc
         res += str(a)
-        chiclo10 //= cc
-        return res[::-1]
+        numb //= cc
+    return res[::-1]
 
-chiclo10 = int(input('Пожалуйста, введите целое число: '))
+numb = int(input('Пожалуйста, введите целое число: '))
 cc = int(input('Пожалуйста, введите основание системы (2 или 8): '))
-print(res[::-1])
+print(f(cc, numb))
